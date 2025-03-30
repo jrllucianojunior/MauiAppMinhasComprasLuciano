9a40857d8a73157a3f7f0ed6d57e14b02fecf1e2
jrllucianojunior <jrl.araujo@hotmail.com>
2025-03-30 15:09:04 -0300

    Agenda 03

---
+++ MauiAppMinhasComprasLuciano/SQLiteDatabaseHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppMinhasComprasLuciano
+{
+    internal class SQLiteDatabaseHelper
+    {
+        private string path;
+
+        public SQLiteDatabaseHelper(string path)
+        {
+            this.path = path;
+        }
+    }
+}
--- MauiAppMinhasComprasLuciano/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasComprasLuciano/Views/NovoProduto.xaml.cs
@@ -1,3 +1,6 @@
+using MauiAppMinhasComprasLuciano.Models;
+using System.Linq.Expressions;
+
 namespace MauiAppMinhasComprasLuciano.Views;
 
 public partial class NovoProduto : ContentPage
@@ -6,4 +9,26 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+            Produto p = new Produto
+            {
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+            };
+
+            await App.Db.Insert(p);
+            await DisplayAlert("Sucesso!", "Registro Inserido", "OK");
+
+        }
+		catch (Exception ex)
+		{
+			DisplayAlert("Ops", ex.Message, "OK");
+		}
+		
+    }
 }
